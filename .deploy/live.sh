c84ef957b73d12328a5519038a6e56e35ada621a
victorlapshev <victor.lapshev@gmail.com>
2019-09-03 00:39:59 +0300

    Some live deployment refactoring

--- .deploy/live.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t excusebot/app .
-docker tag excusebot/app gcr.io/learning-229721/excusebot/app:1.1
-docker push gcr.io/learning-229721/excusebot/app:1.1
-
-kubectl apply -f .deploy/config.yaml
-kubectl apply -f .deploy/app.yaml
-kubectl apply -f .deploy/nginx.yaml
