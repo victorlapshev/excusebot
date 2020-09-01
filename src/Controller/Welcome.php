a851d1e26add05e88eb3fa604972bd7ad1715bac
Victor Lapshev <victor.lapshev@gmail.com>
2020-09-02 02:14:02 +0300

    Remove elastic client initialization on welcome controller

--- src/Controller/Welcome.php
+++ src/Controller/Welcome.php
@@ -2,13 +2,12 @@
 
 namespace App\Controller;
 
-use Elastica\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class Welcome extends AbstractController
 {
-    public function index(Client $client)
+    public function index()
     {
         return new Response('Yo');
     }
